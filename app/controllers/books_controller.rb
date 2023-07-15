9536783676288337d6b58d8ab1d3e9352a20ae13
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-07-15 15:38:12 +0000

    [add] index show edit追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,11 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'book#edit', as: 'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
